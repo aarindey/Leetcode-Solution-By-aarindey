a34d9f62f343f937c306f85ed142f1aac8c756c5
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-06-05 21:19:33 +0530

    Time: 8 ms (6.98%), Space: 6 MB (33.54%) - LeetHub

---
+++ number-complement/number-complement.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int result=0;
+        int i=0;
+        while(num)
+        {
+          if((num&1)==0)
+           result+=(1<<i);
+            i++;
+            num=num>>1;
+        }
+        return result;
+    }
+};
