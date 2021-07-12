444ab876fae507132d6f9eb33e97f5e08299eb99
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-07-13 00:59:35 +0530

    Time: 32 ms (9.89%), Space: 16.6 MB (26.20%) - LeetHub

---
+++ build-array-from-permutation/build-array-from-permutation.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) 
+    {
+        vector<int> temp(nums.size());
+        for(int i=0;i<nums.size();i++)
+        {
+            temp[i]=nums[nums[i]];
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            nums[i]=temp[i];
+        }
+        return nums;
+    }
+};
