0470f8a95ac3733d5cc90fa1572d4644143c51cd
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-07-15 04:41:14 +0530

    Time: 80 ms (21.95%), Space: 368.7 MB (5.17%) - LeetHub

--- implement-strstr/implement-strstr.cpp
+++ implement-strstr/implement-strstr.cpp
@@ -3,8 +3,6 @@ public:
     int strStr(string haystack, string needle) {
         if(needle.size()==0)
          return 0;
-        if(haystack.size()==0)
-         return -1;
         if(haystack.size()<needle.size())
           return -1;  
         int n=needle.size();
