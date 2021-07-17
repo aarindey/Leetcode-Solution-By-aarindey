5df478d095365f9624fee7bab8e7d8526323d2a0
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-07-18 02:49:30 +0530

    Time: 12 ms (60.35%), Space: 12.1 MB (49.91%) - LeetHub

--- longest-nice-substring/longest-nice-substring.cpp
+++ longest-nice-substring/longest-nice-substring.cpp
@@ -1,14 +1,17 @@
-class Solution {
+//1763. Longest Nice Substring
+class Solution 
+{
 public:
 string longestNiceSubstring(string s) {
         if (s.size() < 2) return "";
-        unordered_set<char> ck(begin(s), end(s));
+        unordered_set<char> st(begin(s), end(s));
         for (int i = 0; i < s.size(); i++) {
-            if (ck.find((char) toupper(s[i])) == end(ck) || ck.find((char) tolower(s[i])) == end(ck)) {
+             if (st.find((char) toupper(s[i])) == end(st) || st.find((char) tolower(s[i])) == end(st)) {
                 string s1 = longestNiceSubstring(s.substr(0, i));
                 string s2 = longestNiceSubstring(s.substr(i + 1));
                 return s1.size() >= s2.size() ? s1 : s2;
             }
         }
         return s;
-    }};
+    }
+};
