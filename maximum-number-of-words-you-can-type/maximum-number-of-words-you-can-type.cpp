9cc5d228377879da12deee3dafa7946e79a724ae
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-07-19 03:46:07 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (66.67%) - LeetHub

--- maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
+++ maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -1,35 +1,66 @@
+// //1935. Maximum Number of Words You Can Type
+// class Solution {
+// public:
+//     int canBeTypedWords(string text, string brokenLetters) {
+//      unordered_set<char> s;
+//         for(int i=0;i<brokenLetters.size();i++)
+//         {
+//             s.insert(brokenLetters[i]);
+//         }
+//         int count=0;
+//         text+=' ';
+//         string str;
+//         int left=0;
+//         bool check;
+//         for(int i=0;i<text.length();i++)
+//         {
+//             if(text[i]==' ')
+//             {
+//                 check=true;
+//                 str=text.substr(left,i-left);
+//                 for(int j=0;j<str.length();j++)
+//                 {
+//                     if(s.find(str[j])!=s.end())
+//                     {
+//                         check=false;
+//                         left=i+1;
+//                         break;
+//                     }
+//                     else
+//                     {
+//                         left=i+1;
+//                     }
+//                 }
+//                 if(check)
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
 class Solution {
 public:
+    int c=0,k=1;
     int canBeTypedWords(string text, string brokenLetters) {
-     unordered_set<char> s;
-        for(int i=0;i<brokenLetters.size();i++)
-        {
-            s.insert(brokenLetters[i]);
-        }
-        int count=0;
-        text+=' ';
-        string str;
-        int left=0;
-        bool check;
+        unordered_map<char,bool>map;
+        for(auto i:brokenLetters)
+            map[i]=true;
+        
         for(int i=0;i<text.length();i++)
-        {
             if(text[i]==' ')
-            {
-                check=true;
-                str=text.substr(left,i-left);
-                for(int j=0;j<str.length();j++)
-                {
-                    if(s.find(str[j])!=s.end())
-                    {
-                        check=false;
-                        left=i+1;
-                        break;
-                    }
-                }
-                if(check)
-                count++;
-            }
+                k++;
+        
+        stringstream iss(text);
+        string word;
+        
+        while(iss >> word) {
+            for(int i=0;i<word.size();i++){
+               if(map[word[i]]==true){
+                    k--;
+                     break;   
+               }
+            } 
         }
-        return count;
+        return k;
     }
 };
