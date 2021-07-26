8b98dfd1bb494ec0855d9a01bb4de69b00eacc94
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-07-26 16:42:56 +0530

    Time: 4 ms (100.00%), Space: 6.3 MB (66.67%) - LeetHub

--- sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
+++ sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.cpp
@@ -1,3 +1,4 @@
+//1945. Sum of Digits of String After Convert
 class Solution {
 public:
     int getLucky(string s, int k) {
@@ -7,18 +8,15 @@ public:
         string str=to_string((int)(s[i]-'a'+1));
         num+=str;    
     }
-        long long int n;
-        long long int sum,add=0,res;
+        int n,add=0;
         for(int i=0;i<num.size();i++)
         {
             add+=num[i]-'0';
         } 
         n=add;
-
         for(int i=1;i<k;i++)
         {
             add=0;
-            sum=n;
             while(n>0)
             {
                 add+=n%10;
