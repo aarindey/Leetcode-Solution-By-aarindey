ba5e809572e09e06ca94b931f7233f5222c4699c
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-07-26 18:00:17 +0530

    Time: 8 ms (61.35%), Space: 8.4 MB (89.18%) - LeetHub

---
+++ next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+      int num1;
+        vector<int> ans;
+      for(int i=0;i<nums1.size();i++)
+      {
+          num1=nums1[i];
+          vector<int>::iterator it=find(nums2.begin(),nums2.end(),nums1[i]);
+        while(it!=nums2.end())
+          {
+              if(*it>num1)
+              {
+               ans.push_back(*it);
+               break;
+              }
+              it++;
+          } 
+            if(it==nums2.end())
+           ans.push_back(-1);
+      }   
+      return ans;  
+    }
+};
