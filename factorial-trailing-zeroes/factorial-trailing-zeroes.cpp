119707cd292e833340b3e58d480a63f766118759
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-08-02 01:26:06 +0530

    Time: 4 ms (22.66%), Space: 6 MB (25.45%) - LeetHub

--- factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -2,21 +2,16 @@ class Solution {
 public:
     int trailingZeroes(int n) 
     {
-        int count2=0,count5=0,num;
+        int count5=0,num;
         for(int i=2;i<=n;i++)
         {
            num=i;
-           while(num%2==0)
-           {
-               count2++;
-               num/=2;
-           }
            while(num%5==0)
            {
                count5++;
                num/=5;
            } 
         }   
-        return min(count2,count5);
+        return count5;
     } 
 };
