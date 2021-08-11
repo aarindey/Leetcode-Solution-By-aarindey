3d64c16b3e418c34a87d812388c0814806862f57
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-08-11 08:14:42 +0530

    Time: 4 ms (72.97%), Space: 7.9 MB (40.00%) - LeetHub

--- permutations/permutations.cpp
+++ permutations/permutations.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void helper(vector<int> a,int l,int r) {
+    
+    void helper(vector<vector<int>> &ans,vector<int> a,int l,int r) {
          // Base case
     if (l == r)
      ans.push_back(a);   
@@ -15,7 +15,7 @@ public:
             swap(a[l], a[i]);
  
             // Recursion called
-            helper(a, l+1, r);
+            helper(ans,a, l+1, r);
  
             //backtrack
             swap(a[l], a[i]);
@@ -23,8 +23,9 @@ public:
     } 
     }
     vector<vector<int>> permute(vector<int> &nums)
-    {       
-        helper(nums,0,nums.size()-1);
+    {  
+        vector<vector<int>> ans;
+        helper(ans,nums,0,nums.size()-1);
         return ans;
     }
 };
