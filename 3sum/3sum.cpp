140d197c69ec23e88f6f0ca96ce3313a4b126f69
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-08-27 03:38:33 +0530

    Time: 100 ms (44.97%), Space: 20.1 MB (68.05%) - LeetHub

--- 3sum/3sum.cpp
+++ 3sum/3sum.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-         sort(nums.begin(), nums.end()); 
-           set<vector<int>> res; 
-        if(nums.size() < 3) return {};
-        for(int i = 0; i<nums.size()-2; i++){ 
-                if( i>0 && nums[i]==nums[i-1]) continue; 
-                int j = i+1; int k = nums.size()-1; 
-                while(j!=k){ 
-                 if( nums[i] + nums[j] +nums[k] == 0) 
-                    {
-                        vector<int>v; 
-                     
-                      v.push_back(nums[i]);  
-                       v.push_back(nums[j]);  
-                        v.push_back(nums[k]);
-                     sort(v.begin(),v.end()); 
-                        res.insert(v); 
-                        
-                    } 
-                    if(nums[i] + nums[j] +nums[k] > 0 ) { k--; }
-                    else {  j++; }
-                } 
-                   } 
-        vector< vector< int >> ans; 
-        for(auto a : res) { 
-        ans.push_back(a); 
-        }
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        if(nums.size()<3)
+        return ans;    
+        for(int i=0;i<nums.size()-2;i++)
+        {
+            if(i>0&&nums[i]==nums[i-1])
+            continue;    
+            int low=i+1,high=nums.size()-1;
+            while(low<high)
+            {
+                
+                if(nums[i]+nums[low]+nums[high]==0)
+                {
+                    ans.push_back({nums[i],nums[low],nums[high]});
+                    low++;
+                    while(low<high&&nums[low]==nums[low-1])
+                    low++;    
+                }   
+                else if(nums[i]+nums[low]+nums[high]>0)
+                high--;
+                else
+                low++;    
+                
+            }    
+        }  
         return ans;
     }
 };
