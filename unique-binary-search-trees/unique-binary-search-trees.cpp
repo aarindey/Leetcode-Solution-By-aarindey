42e931fcc391b96669ad9626dbe3579c89d0bc7a
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-09-03 02:53:26 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (98.20%) - LeetHub

--- unique-binary-search-trees/unique-binary-search-trees.cpp
+++ unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,16 +1,11 @@
 class Solution {
-public:
+    public:
     int numTrees(int n) {
-    int c[n+1];
-    c[0]=c[1]=1;    
-    for(int i=2;i<=n;i++)
-    {
-        c[i]=0;
-        for(int j=0;j<i;j++)
+        long long ans=1;
+        for(int i=n+1;i<=2*n;i++)
         {
-            c[i]+=c[j]*c[i-j-1];
+            ans=(ans*i)/(i-n);
         }
-    }
-        return c[n];
+        return ans/(n+1);
     }
 };
