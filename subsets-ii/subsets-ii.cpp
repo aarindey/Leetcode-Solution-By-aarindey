90cef5b54f48ab14704a7bfde3ca7b28a3e0111b
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-09-06 21:22:07 +0530

    Time: 12 ms (12.45%), Space: 11.4 MB (22.85%) - LeetHub

---
+++ subsets-ii/subsets-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void recurse(int i,vector<int> &v,vector<vector<int>> &ans,vector<int>& nums)
+    {
+        if(i==nums.size())
+        {
+            if(find(ans.begin(),ans.end(),v)==ans.end())
+            ans.push_back(v);
+            return;
+        }   
+        recurse(i+1,v,ans,nums);
+        v.push_back(nums[i]);
+        recurse(i+1,v,ans,nums);
+        v.pop_back();
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> empty;
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        recurse(0,empty,ans,nums);
+        return ans;
+    }
+};
