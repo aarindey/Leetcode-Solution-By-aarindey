1a05ad47f47cc4e930fa9af2e90b30e4a7acebd0
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-09-10 00:54:50 +0530

    Time: 8 ms (92.81%), Space: 10.7 MB (81.65%) - LeetHub

--- reshape-the-matrix/reshape-the-matrix.cpp
+++ reshape-the-matrix/reshape-the-matrix.cpp
@@ -1,25 +1,47 @@
-class Solution {
-public:
-    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-     int n=mat.size(),m=mat[0].size(),k=0;
-    if(c*r!=n*m)
-        return mat;
-        vector<vector<int>> ans;
-        vector<int> v;
-     for(int i=0;i<n;i++)
-        {
-         for(int j=0;j<m;j++)
+// class Solution {
+// public:
+//     vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+//         int m = nums.size(), n = nums[0].size(), o = m * n;
+//         if (r * c != o) return nums;
+//         vector<vector<int>> res(r, vector<int>(c, 0));
+//         for (int i = 0; i < o; i++) res[i / c][i % c] = nums[i / n][i % n];
+//         return res;
+//     }
+// };
+// class Solution {
+// public:
+//     vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+//         int m = nums.size(), n = nums[0].size();
+//         if (m * n != r * c) {
+//             return nums;
+//         }
+
+//         vector<vector<int>> res(r, vector<int>(c, 0));
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 int k = i * n + j;
+//                 res[k / c][k % c] = nums[i][j];
+//             }
+//         }
+
+//         return res;
+//     }
+// };
+class Solution{
+    public:
+     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+      int n=mat.size(),m=mat[0].size();
+         if(m*n!=r*c)
+          return mat;
+          vector<vector<int>> ans(r, vector<int>(c, 0));
+         for(int i=0;i<n;i++)
          {
-         v.push_back(mat[i][j]);
-             k++;
-             if(k==c)
+             for(int j=0;j<m;j++)
              {
-                k=0; 
-                ans.push_back(v); 
-                v.clear();    
+                int k=m*i+j;
+                 ans[k/c][k%c]=mat[i][j];
              }
-        }
+         }
+         return ans;
      }
-        return ans;
-    }
 };
