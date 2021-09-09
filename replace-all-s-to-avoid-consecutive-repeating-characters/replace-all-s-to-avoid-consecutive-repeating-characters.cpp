45edbdd5dd443cf45ad7ac0879ec14495390eb88
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-09-10 01:37:17 +0530

    Time: 3 ms (38.33%), Space: 5.9 MB (88.44%) - LeetHub

--- replace-all-s-to-avoid-consecutive-repeating-characters/replace-all-s-to-avoid-consecutive-repeating-characters.cpp
+++ replace-all-s-to-avoid-consecutive-repeating-characters/replace-all-s-to-avoid-consecutive-repeating-characters.cpp
@@ -1,47 +1,17 @@
 class Solution {
 public:
     string modifyString(string s) {
-     string alpha="abcdefghijklmnopqrstuvwxyz";
-        
         int j=0,n=s.length();
-        if(n==1)
-        {
-            if(s[0]=='?')
-             return "a";
-            else
-             return s;
-        }   
+    
      for(int i=0;i<n;i++)
      {
          if(s[i]=='?')
          {
-             if(i==0)
-             {
-                 if(alpha[j]==s[i+1])
-                 {
-                     j++;
-                 }
-                 s[i]=alpha[j];
-                 j=0;
-             }
-             else if(i==n-1)
-             {
-               if(alpha[j]==s[i-1])
-                 {
-                     j++;
-                 }
-                 s[i]=alpha[j];
-                 j=0;  
-             }
-             else
-             {
-                 while(alpha[j]==s[i-1]||alpha[j]==s[i+1])
-                 {
-                     j++;
-                 }
-                 s[i]=alpha[j];
-                 j=0;
-             }
+            for(s[i]='a';s[i]<='c';s[i]++)
+            {
+                if((i==0||s[i]!=s[i-1])&&(i==n-1||s[i+1]!=s[i]))
+                 break;   
+            }
          }
      }
         return s;
