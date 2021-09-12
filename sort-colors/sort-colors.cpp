a475ace56bbaec0023ae94872ae0b074f1087fa8
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-09-12 18:59:00 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (22.83%) - LeetHub

--- sort-colors/sort-colors.cpp
+++ sort-colors/sort-colors.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-sort(nums.begin(),nums.end());
-    }
+int l=0,mid=0,r=nums.size()-1;
+        while(mid<=r)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[l++],nums[mid++]);
+            }
+            else if(nums[mid]==2)
+            {
+                swap(nums[mid],nums[r--]);
+            }
+            else
+            {
+                mid++;
+            }  
+        }   
+}
 };
