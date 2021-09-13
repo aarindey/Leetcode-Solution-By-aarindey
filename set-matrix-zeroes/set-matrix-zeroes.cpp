c4aeac061a71d30364889ac282974b59450cb0f5
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-09-13 23:19:07 +0530

    Time: 12 ms (86.07%), Space: 13 MB (95.95%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.cpp
+++ set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,53 +1,38 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        if(m==0||n==0)
-            return;
-        bool firstrow=false,firstcol=false;
+    int n=matrix.size(),m=matrix[0].size();
+         int flag = -100000;
         for(int i=0;i<n;i++)
         {
-            if(matrix[0][i]==0)
-             firstrow=true;   
-        }  
-        for(int i=0;i<m;i++)
-        {
-            if(matrix[i][0]==0)
-              firstcol=true;  
-        }
-        for(int i=1;i<m;i++)
-        {
-            for(int j=1;j<n;j++)
+            for(int j=0;j<m;j++)
             {
                 if(matrix[i][j]==0)
                 {
-                    matrix[i][0]=0;
-                     matrix[0][j]=0;   
+                    int row=i,col=j;
+                    for(int k=0;k<n;k++)
+                    {
+                        if(matrix[k][col]!=0)
+                        matrix[k][col]=flag;
+                    }
+                      for(int k=0;k<m;k++)
+                    {
+                        if(matrix[row][k]!=0)  
+                        matrix[row][k]=flag;
+                    }
                 }
             }
         }
-        for(int i=1;i<m;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=1;j<n;j++)
+            for(int j=0;j<m;j++)
             {
-                if(matrix[i][0]==0||matrix[0][j]==0)
-                  matrix[i][j]=0;  
-            }
-        }
-        if(firstrow==true)
-        {
-            for(int i=0;i<n;i++)
-            {
-                matrix[0][i]=0;
-            }
-        }  
-        if(firstcol==true)
-        {
-            for(int i=0;i<m;i++)
-            {
-                matrix[i][0]=0;
+                if(matrix[i][j]==flag)
+                {
+                    matrix[i][j]=0;
+                }
             }
         }
     }
 };
+ 
