ab8acc78d5a01542d0c2b288b93395ee6c27352a
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-10-07 00:47:55 +0530

    Time: 8 ms (61.68%), Space: 9.2 MB (28.78%) - LeetHub

---
+++ string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+      vector<string> ans;
+      for(auto i:words)
+      {
+          for(auto j:words)
+          {
+              if(i!=j&&j.find(i)!=-1)
+              {
+                  ans.push_back(i);
+                  break;
+              }
+          }
+      }
+    return ans;
+    }
+};
