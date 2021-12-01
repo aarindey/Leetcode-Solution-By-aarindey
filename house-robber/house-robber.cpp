702cf44e2d30bf65e1170e746c378fd77aa0d22e
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-12-01 19:08:47 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (22.00%) - LeetHub

--- house-robber/house-robber.cpp
+++ house-robber/house-robber.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
-    int recurse(vector<int> &nums,int i,int prev,vector<int> &dp)
-    {
-        if(nums.size()==0)
-        return 0;
-        if(i>=nums.size())
-        return 0;
-        if(dp[i]!=-1)
-        return dp[i];
-        if(prev!=-1&&abs(i-prev)==1)
-        {
-        return dp[i]=recurse(nums,i+1,prev,dp);
-        }
-        else
-        {
-            return dp[i]=max(nums[i]+recurse(nums,i+2,i,dp),recurse(nums,i+1,prev,dp));
-        }
-    }
     int rob(vector<int>& nums) {
-     vector<int> dp(nums.size()+1,-1);
-     int ans=recurse(nums,0,-1,dp);
-      return ans;
+        int n=nums.size();
+        if(n==1)
+        return nums[0];
+        vector<int> dp(nums.size());
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+     for(int i=2;i<n;i++)
+     {
+         dp[i]=max(dp[i-2]+nums[i],dp[i-1]);
+     }
+      return dp[n-1];
     }
 };
