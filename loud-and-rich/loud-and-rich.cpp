6b3481f02736e38d20549af6136fb69324e62e26
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-12-09 03:33:54 +0530

    Time: 94 ms (74.60%), Space: 42.6 MB (55.98%) - LeetHub

--- loud-and-rich/loud-and-rich.cpp
+++ loud-and-rich/loud-and-rich.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    int dfs(int node,vector<vector<int> > &graph,vector<int> quiet,vector<int> &ans)
-    {
-        if(ans[node]!=-1)
-        return ans[node];
-        else
-        {
-        ans[node]=node;
-        for(auto child:graph[node])
-        {
-            if(quiet[dfs(child,graph,quiet,ans)]<quiet[ans[node]])
-            {
-                ans[node]=ans[child];
-            }
-        }
-        }
-        return ans[node];
-    }
     vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
-        //constructing the graph
         int n=quiet.size();
-        vector<vector<int>> graph(n);
-        for(int i=0;i<richer.size();i++)
+        vector<int> indegree(n,0);
+        vector<int> ans(n);
+        for(int i=0;i<n;i++)
+        ans[i]=i;
+        vector<vector<int> > graph(n);
+        for(auto &x:richer)
         {
-            graph[richer[i][1]].push_back(richer[i][0]);
+            graph[x[0]].push_back(x[1]);
+            indegree[x[1]]++;
         }
-        vector<int> ans(n,-1);
+        queue<int> q;
         for(int i=0;i<n;i++)
         {
-            dfs(i,graph,quiet,ans);
+            if(indegree[i]==0)
+            q.push(i);
+        }
+        while(!q.empty())
+        {
+            int element=q.front();
+            q.pop();
+            for(int node:graph[element])
+            {
+                if(quiet[ans[node]]>quiet[ans[element]])
+                {
+                    ans[node]=ans[element];
+                }
+                indegree[node]--;
+                if(indegree[node]==0)
+                q.push(node);
+            }
         }
         return ans;
     }
