cb5bd18469de297dc0913bf6d68e0e4ded554e7f
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-12-09 20:48:48 +0530

    Time: 12 ms (78.62%), Space: 9.8 MB (45.45%) - LeetHub

--- minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>& matrix,int c,int r,vector<vector<int>> &visited)
-    {
-     int sum=0;
-     if(c>=matrix.size()||c<0||r>=matrix.size())
-     return 0;
-     if(visited[r][c]!=-1)
-     return visited[r][c];
-     sum+=matrix[r][c];
-     if(c==0)
-     {
-        sum+=min(dfs(matrix,c,r+1,visited),dfs(matrix,c+1,r+1,visited));
-     }
-     else if(c==matrix.size()-1)
-     {
-        sum+=min(dfs(matrix,c,r+1,visited),dfs(matrix,c-1,r+1,visited));
-     }
-     else
-     {
-        sum+=min(dfs(matrix,c,r+1,visited),min(dfs(matrix,c-1,r+1,visited),dfs(matrix,c+1,r+1,visited)));
-     }
-    visited[r][c]=sum;
-    return sum;
-    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int mini=INT_MAX;
         int n=matrix.size();
-        vector<vector<int> > visited(n,vector<int>(n,-1));
-        for(int i=0;i<n;i++)
+        int mini=INT_MAX;
+        if(n==1)
+        return matrix[0][0];
+        if(n==0)
+        return 0;
+        for(int r=1;r<n;r++)
         {
-            mini=min(mini,dfs(matrix,i,0,visited));
+            for(int c=0;c<n;c++)
+            {
+                if(c==0)
+                {
+                    matrix[r][c]+=min(matrix[r-1][c],matrix[r-1][c+1]);
+                }
+                else if(c==n-1)
+                {
+                    matrix[r][c]+=min(matrix[r-1][c],matrix[r-1][c-1]);
+                }
+                else
+                {
+                    matrix[r][c]+=min(matrix[r-1][c],min(matrix[r-1][c+1],matrix[r-1][c-1]));
+                }
+                if(r==n-1)
+                {
+                    mini=min(mini,matrix[r][c]);
+                }
+            }
         }
         return mini;
     }
