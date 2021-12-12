b90efc67fe378a4ac8f3e74eaed7c9a87886f00c
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-12-12 06:01:42 +0530

    Time: 168 ms (50.03%), Space: 9.1 MB (97.10%) - LeetHub

--- partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,31 +1,59 @@
 class Solution {
 public:
-bool isSubsetSum(int N, vector<int> &arr, int sum){
-       vector<vector<bool>> dp(N+1,vector<bool> (sum+1));
-       for(int i=0;i<=N;i++)
-       {
-           for(int j=0;j<=sum;j++)
-           {
-               if(j==0)
-               dp[i][j]=true;
-               else if(i==0)
-               dp[i][j]=false;
-               else if(arr[i-1]<=j)
-               {
-                   dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
-               }
-               else
-               dp[i][j]=dp[i-1][j];
-           }
-       }
-       return dp[N][sum];
-    }
-    bool canPartition(vector<int>& nums) {
-        int sum=accumulate(nums.begin(),nums.end(),0);
-        if(sum%2!=0)
+bool canPartition(vector<int> &nums) {
+
+    int n=nums.size();
+    
+    int sum=accumulate(nums.begin(),nums.end(),0);
+    
+    // We have to divide the whole array into two non-empty subsets .
+    
+    // Again sum of subset1 == sum of subset2
+    
+    // That means sum of subset1 + sum of subset2 == sum of array
+    
+    // 2 * sum of subset1 == sum of array
+    
+    // Sum of subset1 == Sum of subset2 == ( sum of array / 2 )
+    
+    // Which clearly shows that the sum of array elements should be even.
+    
+    // Edge Case Handling.
+    
+    if(sum%2!=0){
         return false;
-        else
-        sum/=2;
-        return isSubsetSum(nums.size(),nums,sum);
     }
+    
+    int target=sum/2;
+    
+    
+    // Now we have to figure out whether we can form (target) by using one or more than one elements , as If we able to form target then it is already proved that (sum of array - target) automatically goes to another subset.
+    
+    
+    vector<bool>dp(target+1);
+    
+    // Base Condition , as we can get 0 without disturbing any of the array element.
+    
+    dp[0]=true;
+    
+    // Most Popular Way to solve these type of problem is using DP.
+    
+    for(int i=0;i<n;i++){
+        
+        
+        // Now for the first time it arises doubt that why the inner loop runs in backward direction.
+        
+        // Just do a dry run first running the loop in backward direction and then in forward direction. Then it is crystal clear to you.
+        
+        for(int j=target;j>=nums[i];j--){
+            
+            dp[j]=dp[j] || dp[j-nums[i]];
+            
+        }
+        
+    }
+    
+    return dp[target];
+    
+}
 };
