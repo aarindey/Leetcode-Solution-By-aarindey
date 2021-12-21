45063d82247d8bf1e7d8935dbc82965f2c033a68
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-12-21 10:28:03 +0530

    Time: 8 ms (94.47%), Space: 10.5 MB (55.79%) - LeetHub

--- 3sum-with-multiplicity/3sum-with-multiplicity.cpp
+++ 3sum-with-multiplicity/3sum-with-multiplicity.cpp
@@ -2,26 +2,35 @@ class Solution {
 public:
     long long int mod=1e9+7;
     int threeSumMulti(vector<int>& arr, int target) {
-      int n=arr.size();
-      vector<long long> c(101,0);
-        for(int i=0;i<n;i++)
-        c[arr[i]]++;
-        long long int result=0;
-        for(int i = 0; i <= 100; i++) {
-            for(int j = i; j <= 100; j++) {
-                int k = target-i-j;
-                if(k<0 || k >100) continue;
-                if(i == j && j==k) {
-                    result+= (c[i] * (c[i]-1) * (c[i]-2) /6);
+        unordered_map<int,long> mp;
+        for(int &x:arr)
+        {
+            mp[x]++;
+        }
+        long long int ans=0;
+        for(auto &pr:mp)
+        {
+            int i=pr.first;
+            for(auto &pr2:mp)
+            {
+                int j=pr2.first;
+                int k=target-i-j;
+                if(k<0||k>100||mp.find(k)==mp.end())
+                continue;
+                if(i==j&&j==k)
+                {
+                    ans+=mp[i]*(mp[i]-1)*(mp[i]-2)/6;
                 }
-                else if(i == j && j != k) {
-                    result += ((c[i] * (c[i]-1) /2 ) * c[k]);
+                else if(i==j&&j!=k)
+                {
+                    ans+=mp[k]*mp[i]*(mp[i]-1)/2;
                 }
-                else if(i < j && j < k) {
-                    result += (c[i] * c[j] * c[k]);
+                else if(i<j&&j<k)
+                {
+                    ans+=mp[i]*mp[j]*mp[k];
                 }
             }
         }
-        return (int)(result%mod);
+        return (int)(ans%mod);
     }
 };
