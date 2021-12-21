f17d59fd87d282b6332ccad795171f224bc98d5d
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-12-21 10:18:34 +0530

    Time: 4 ms (99.21%), Space: 10.6 MB (41.32%) - LeetHub

--- 3sum-with-multiplicity/3sum-with-multiplicity.cpp
+++ 3sum-with-multiplicity/3sum-with-multiplicity.cpp
@@ -3,23 +3,25 @@ public:
     long long int mod=1e9+7;
     int threeSumMulti(vector<int>& arr, int target) {
       int n=arr.size();
-     
-      long long c=0;
-      for(int i=0;i<n;i++)
-      {
-          vector<int> count(101,0);
-          
-          for(int j=i+1;j<n;j++)
-          {
-              int k=target-arr[i]-arr[j];
-              if(k>=0&&k<=100&&count[k]>0)
-              {
-                  c+=count[k];
-                  c%=mod;
-              }
-              count[arr[j]]++;
-          }
-      }
-        return (int)c;
+      vector<long long> c(101,0);
+        for(int i=0;i<n;i++)
+        c[arr[i]]++;
+        long long int result=0;
+        for(int i = 0; i <= 100; i++) {
+            for(int j = i; j <= 100; j++) {
+                int k = target-i-j;
+                if(k<0 || k >100) continue;
+                if(i == j && j==k) {
+                    result+= (c[i] * (c[i]-1) * (c[i]-2) /6);
+                }
+                else if(i == j && j != k) {
+                    result += ((c[i] * (c[i]-1) /2 ) * c[k]);
+                }
+                else if(i < j && j < k) {
+                    result += (c[i] * c[j] * c[k]);
+                }
+            }
+        }
+        return (int)(result%mod);
     }
 };
