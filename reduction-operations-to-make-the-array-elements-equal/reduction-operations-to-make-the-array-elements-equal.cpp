247866dab71001ea4d2fe731373cf62d9ecaddab
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2021-12-28 03:31:05 +0530

    Time: 188 ms (76.14%), Space: 82.7 MB (92.48%) - LeetHub

--- reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
+++ reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int reductionOperations(vector<int>& nums) {
-       int ans=0;
+        int ans=0;
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        for(int i=1;i<n;i++)
+        for(int i=n-1;i>=1;i--)
         {
-            if(nums[i-1]<nums[i])
-            {
-                ans+=n-i;
-            }
+            if(nums[i]>nums[i-1])
+            ans+=n-i;
         }
         return ans;
     }
