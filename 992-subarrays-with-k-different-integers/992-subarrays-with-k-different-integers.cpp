f676fc6ffcfbdff01227acb6bc8032e7612bc290
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-01-07 08:02:22 +0530

    Time: 296 ms (6.53%), Space: 46.1 MB (56.52%) - LeetHub

---
+++ 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int helper(vector<int> &nums,int k)
+    {
+     if(k==0)
+     return 0;
+     
+     int j=0;
+     int n=nums.size();
+     map<int,int> freq;
+     int total=0;
+     int diff=0;
+     for(int i=0;i<n;i++)
+     {
+        if(freq[nums[i]]==0)
+        diff++;
+        freq[nums[i]]++;
+        if(diff<=k)
+        {
+           total+=(i-j+1);
+        }
+        else
+        {
+            while(j<n&&j<=i&&diff>k)
+            {
+                freq[nums[j]]--;
+                if(freq[nums[j]]==0)
+                diff--;
+                j++;
+            }
+            total+=(i-j+1);
+        }
+     }
+     return total;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return helper(nums,k)-helper(nums,k-1);
+    }
+};
