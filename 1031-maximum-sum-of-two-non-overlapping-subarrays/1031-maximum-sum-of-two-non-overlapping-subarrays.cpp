ed7030bb75d3116e57c68e615f28f90643493ba4
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-01-10 00:52:55 +0530

    Time: 4 ms (77.23%), Space: 8.9 MB (11.47%) - LeetHub

---
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int helper(vector<int> &nums,int x,int y)
+    {
+        int n=nums.size();
+        vector<int> dp1(n,0),dp2(n,0);
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+           if(i<x)
+           {
+               sum+=nums[i];
+               dp1[i]=sum;
+           }
+           else
+           {
+               sum+=nums[i]-nums[i-x];
+               dp1[i]=max(sum,dp1[i-1]);
+           }
+        }
+        sum=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(i+y>=n)
+            {
+                sum+=nums[i];
+                dp2[i]=sum;
+            }
+            else
+            {
+                 sum+=nums[i]-nums[i+y];
+                 dp2[i]=max(sum,dp2[i+1]);
+            }
+        }
+        int ans=0;
+        for(int i=x-1;i<n-y;i++)
+        {
+           ans=max(ans,dp1[i]+dp2[i+1]); 
+        }
+        return ans;
+    }
+    int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
+        return max(helper(nums,firstLen,secondLen),helper(nums,secondLen,firstLen));
+    }
+};
