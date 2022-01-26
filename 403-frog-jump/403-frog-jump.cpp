cd36f09bbf2eac0b540eb93d5d4597788c1aebe4
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-01-26 23:42:15 +0530

    Time: 487 ms (16.37%), Space: 54 MB (30.26%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool canCross(vector<int>& stones) {
-        map<int, set<int>> m;
-        m[stones[0]+1] = {1}; 
+        map<int, set<int>> mp;
+        mp[stones[0]+1] = {1}; 
         for(int i =1;i<stones.size();i++){
-            for(auto jump : m[stones[i]]){
-                m[stones[i]+jump].insert(jump);
-                m[stones[i]+jump-1].insert( jump-1);
-                m[stones[i]+jump+1].insert( jump+1);
+            for(auto jump : mp[stones[i]]){
+                mp[stones[i]+jump].insert(jump);
+                mp[stones[i]+jump-1].insert( jump-1);
+                mp[stones[i]+jump+1].insert( jump+1);
             }
             
         }
-        if(m[(stones[stones.size()-1])].size())
+        if(mp[stones[stones.size()-1]].size())
         return true; 
         return false;
     }
