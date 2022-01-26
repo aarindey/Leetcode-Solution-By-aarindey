950ebaa3f29a6f9d775873a218e30f9084d833f9
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-01-26 23:40:29 +0530

    Time: 422 ms (19.79%), Space: 51.7 MB (34.98%) - LeetHub

--- 403-frog-jump/403-frog-jump.cpp
+++ 403-frog-jump/403-frog-jump.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     bool canCross(vector<int>& stones) {
-        map<int,set<int>> mp;
-        int n=stones.size();
-        mp[stones[0]].insert(1);
-        for(auto pr:mp)
-        {
-            int currPos=pr.first;
-            int pos;
-            for(auto it=pr.second.begin();it!=pr.second.end();it++)
-            {
-                pos=currPos+*it;
-                if(pos==stones[n-1])
-                return true;
-                if(find(stones.begin(),stones.end(),pos)!=stones.end())
-                {
-                    if(*it-1>0)
-                    mp[pos].insert(*it-1);
-                    mp[pos].insert(*it);
-                    mp[pos].insert(*it+1);
-                }
+//      using map that stores number and the lenghts of jumps we took to reach there
+//         for eg m[3] = {2} this means we reached 3 by jumping with k units. we don't care where we jumped from, we just know,from 3 we can go to 3+2-1, 3+2,3+2+1 points. we store corresponding jumplengths for these numbers too. 
+//         at the end we check the last element, if we were able to reach it, the jump lenght wd be stored so return true;
+        
+        unordered_map<int, unordered_set<int>> m;
+        m[stones[0]+1] = {1}; //becaause from 0th postion we can jump by only 1 unit
+        for(int i =1;i<stones.size();i++){
+            for(auto jump : m[stones[i]]){
+//                 types of jumps we took to reach stones[i],
+                // for eg m[4] = {2,3} that means we can jump with k =2 and k=3 
+                m[stones[i]+jump].insert(jump);
+                m[stones[i]+jump-1].insert( jump-1);
+                m[stones[i]+jump+1].insert( jump+1);
             }
+            
         }
+        if(m[(stones[stones.size()-1])].size())
+            return true; //means we somehow jumped to the end stone
         return false;
     }
 };
