842e9a776253d16b74aba28666b22a5197b33c81
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-01-30 15:24:55 +0530

    Time: 534 ms (36.36%), Space: 286.7 MB (22.73%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
@@ -5,16 +5,23 @@ public:
         int total_ones=accumulate(nums.begin(),nums.end(),0);
         int c=total_ones;
         int num;
-        unordered_map<int,vector<int>> mp;
-        mp[c].push_back(0);
+        vector<int> ans;
         int curr_ones=0;
+        ans={0};
         for(int i=1;i<=n;i++)
         {
         curr_ones+=nums[i-1];
         num=i-curr_ones+total_ones-curr_ones;
-        c=max(c,num);
-        mp[num].push_back(i);
+        if(num>c)
+        {
+            ans={i};
+            c=num;
         }
-        return mp[c];
+        else if(num==c)
+        {
+            ans.push_back(i);
+        }
+        }
+        return ans;
     }
 };
