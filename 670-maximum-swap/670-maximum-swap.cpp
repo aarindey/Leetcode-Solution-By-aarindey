71d36936093e1446f55b38cd26eb2732893c854c
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-02-17 20:03:26 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (61.16%) - LeetHub

---
+++ 670-maximum-swap/670-maximum-swap.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        vector<int> digits;
+        int maxi=INT_MIN;
+        vector<int> maxArray;
+        int i=0,idx;
+        while(num>0)
+        {
+            digits.push_back(num%10);
+            if(num%10>maxi)
+            {
+            maxi=num%10;
+            idx=i;
+            }
+            maxArray.push_back(idx);
+            num/=10;
+            i++;
+        }
+        int n=digits.size();
+        
+        for(int i=n-1;i>=0;i--)
+        {
+         if(digits[maxArray[i]]!=digits[i])
+         {
+           int temp=digits[maxArray[i]];
+           digits[maxArray[i]]=digits[i];
+           digits[i]=temp;
+           break;
+         }
+        }
+        num=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            num=num*10+digits[i];
+        }
+        return num;
+    }
+};
