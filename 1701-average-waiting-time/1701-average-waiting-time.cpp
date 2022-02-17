b5c11518de3bbfcdb9c96864a0153e154b837cde
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-02-18 02:16:14 +0530

    Time: 368 ms (32.61%), Space: 82.9 MB (5.28%) - LeetHub

---
+++ 1701-average-waiting-time/1701-average-waiting-time.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+   double ans;
+    int n=customers.size();
+    int currTime=customers[0][0];
+    double total=0;
+   for(auto customer:customers)
+   {
+       
+        if(currTime>=customer[0])
+        {
+        currTime+=customer[1];
+        total+=currTime-customer[0];
+        }
+        else
+       {
+       total+=customer[1];     
+       currTime=customer[0]+customer[1];
+       }
+   }
+     ans=(double)(total/(1.0*n));
+        return ans;
+    }
+};
