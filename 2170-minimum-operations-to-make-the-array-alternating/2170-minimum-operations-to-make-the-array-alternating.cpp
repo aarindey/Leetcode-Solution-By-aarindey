c300155ed2a183fc28402c59b7fc6db34d4f2514
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-02-17 11:42:40 +0530

    Time: 625 ms (38.05%), Space: 153.3 MB (27.99%) - LeetHub

--- 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.cpp
+++ 2170-minimum-operations-to-make-the-array-alternating/2170-minimum-operations-to-make-the-array-alternating.cpp
@@ -2,38 +2,36 @@ class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
      unordered_map<int,int> mp1,mp2;
-        int c1=0,c2=0,c3=0,c4=0;;
         int n=nums.size();
-        int num1,num2;
+        if(n==1)
+        return 0;
         for(int i=0;i<n;i++)
         {
             if(i%2==0)
             {
                 mp1[nums[i]]++;
-                if(mp1[nums[i]]>c1)
-                {
-                    c1=mp1[nums[i]];
-                    num1=nums[i];
-                }else if(nums[i]!=num1)
-                {
-                    c3=max(c3,mp1[nums[i]]);
-                }
             }else{
                 mp2[nums[i]]++;
-                if(mp2[nums[i]]>c2)
-                {
-                    c2=mp2[nums[i]];
-                    num2=nums[i];
-                }else if(nums[i]!=num2)
-                {
-                    c4=max(c4,mp2[nums[i]]);
-                }
             }
         }
-        if(num1!=num2)
-        return n-c1-c2;
-        
-        
-        return min(n-c1-c4,n-c3-c2);
+        vector<pair<int,int>> odd,even;
+        for(auto pr:mp1)
+        {
+            even.push_back({pr.second,pr.first});
+        }
+        for(auto pr:mp2)
+        {
+            odd.push_back({pr.second,pr.first});
+        }
+        sort(even.begin(),even.end(),greater<pair<int,int>>());
+        sort(odd.begin(),odd.end(),greater<pair<int,int>>());
+        if(odd[0].second!=even[0].second)
+        return n-(odd[0].first+even[0].first);
+        int even1,even2,odd1,odd2;
+        odd1=odd[0].first;
+        even1=even[0].first;
+        odd2=(odd.size()==1)?0:odd[1].first;
+        even2=(even.size()==1)?0:even[1].first;
+        return min(n-(odd1+even2),n-(even1+odd2));
     }
 };
