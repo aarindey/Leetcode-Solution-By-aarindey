5f73bab056cefa4ea17fbbda751a571c55735f0f
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-02-18 01:18:00 +0530

    Time: 244 ms (46.85%), Space: 10 MB (44.58%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool helper(vector<int> &match,vector<int> &side,int i)
+    {
+        if(i==match.size())
+        {
+            if(side[0]==0&&side[1]==0&&side[2]==0&&side[3]==0)
+                return true;
+            else
+                return false;
+        }
+        for(int j=0;j<4;j++)
+        {
+            if(match[i]>side[j])
+            {
+                continue;
+            }
+            side[j]-=match[i];
+            if(helper(match,side,i+1))
+            return true;
+            side[j]+=match[i];
+        }
+        return false;
+    }
+    bool makesquare(vector<int>& matchsticks) {
+        int sum=accumulate(matchsticks.begin(),matchsticks.end(),(int)0);
+        int n=matchsticks.size();
+        if(sum%4!=0||n<4)
+        return false;
+        vector<int> side(4,sum/4);
+        sort(matchsticks.begin(),matchsticks.end(),greater<int>());
+        return helper(matchsticks,side,0);
+    }
+};
