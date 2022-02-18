16fa0bee34a64cfdf07085f40d586e0165bb66b3
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-02-19 00:35:49 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (48.22%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-      stack<char> st;
         int n=s.length();
+        int left=0,right=0;
         for(int i=0;i<n;i++)
-        {  
-             if(!st.empty()&&st.top()=='('&&s[i]==')')
-             {
-                 st.pop();
-                 continue;
-             }
-             
-             st.push(s[i]); 
-         }
-    return st.size();
+        {
+            if(s[i]=='(')
+            {
+                right++;
+            }
+            else
+            {
+                if(right>0)
+                right--;
+                else
+                left++;
+            }
+        }
+        return left+right;
     }
 };
