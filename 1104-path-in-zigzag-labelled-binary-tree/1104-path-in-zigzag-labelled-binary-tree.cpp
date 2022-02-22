9a8733771fd1d0110980df9ba38ae491cec0ae38
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-02-22 19:29:04 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (37.59%) - LeetHub

---
+++ 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> pathInZigZagTree(int label) {
+        int height=log2(label);
+        vector<int> ans;
+        ans.push_back(label);
+        while(height!=0)
+        {
+            int left=pow(2,height-1);
+            int right=pow(2,height)-1;
+            label=left+(right-label/2);
+            ans.push_back(label);
+            height--;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
