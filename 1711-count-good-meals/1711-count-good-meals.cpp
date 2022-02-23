24fade274644d1035549fe61388b2a022525e5c4
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-02-23 09:20:41 +0530

    Time: 352 ms (86.60%), Space: 60.4 MB (26.66%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.cpp
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -5,10 +5,19 @@ public:
         ll mod=1e9+7;
         unordered_map<ll,ll> mp2;
         ll n=d.size(),ans=0;
+        sort(d.begin(),d.end());
+        ll maxTwo;
+        if(n>=2)
+        {
+            maxTwo=log2(d[n-1]+d[n-2]);
+        }else
+        {
+            maxTwo=log2(d[n-1]);
+        }
         for(ll i=0;i<n;i++)
         {
             if(!mp2.empty())
-            for(ll j=0;j<=21;j++)
+            for(ll j=0;j<=maxTwo;j++)
             {
                 ll x=(ll)pow(2,j)-d[i];
                 if(mp2.find(x)!=mp2.end())
