39941f711cb8b5dd786cc0ce96831939d80a068d
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-03-24 10:00:37 +0530

    Time: 99 ms (65.89%), Space: 42 MB (10.43%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -6,12 +6,17 @@ public:
         int i=0,j=n-1;
         while(i<=j)
         {
-            c++;
             if(people[i]+people[j]<=limit)
             {
                 i++;
+                j--;
+                c++;
             }
+            else
+            {
             j--;
+            c++;
+            }
         }
         return c;
     }
