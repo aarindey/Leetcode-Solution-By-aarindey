92f3d79c1472fb0bd4f00272d86c10593dfc9818
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-03-28 14:43:17 +0530

    Added solution - LeetHub

---
+++ Swap bits - GFG/swap-bits.cpp
@@ -0,0 +1,39 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+public:    
+    int swapBits(int x, int p1, int p2, int n)
+    {
+        // Code Here
+bitset<32> s(x);
+for(int i=0;i<n;i++)
+{
+    int t=s[p1+i];
+    s[p1+i]=s[p2+i];
+    s[p2+i]=t;
+}
+return s.to_ullong();
+    }
+};
+
+// { Driver Code Starts.
+
+ 
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int x,p1,p2,n;
+		cin>>x>>p1>>p2>>n;
+		Solution obj;
+		int res=obj.swapBits(x,p1,p2,n);
+		printf("%d\n", res);
+	}
+    return 0;
+}  // } Driver Code Ends
