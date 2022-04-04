ff22bfafbafb31bbe7d096aaf5538c0fe074bb02
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-04-04 16:41:40 +0530

    Time: 6 ms (40.47%), Space: 6.2 MB (76.00%) - LeetHub

--- 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    int countSubstrings(string s, string t) {
-        int ans=0;
-        int n=s.length();
-        int m=t.length();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                int miss=0;
-                for(int p=0;i+p<n&&j+p<m;p++)
-                {
-                    if(s[i+p]!=t[j+p])
-                    {
-                        miss++;
-                        if(miss>=2)
-                        break;
-                    }
-                    ans+=miss;
-                }
-            }
-        }
-        return ans;
-    }
+    int countSubstrings(string &s, string &t) {
+    int res = 0;
+    int dpl[101][101] = {}, dpr[101][101] = {};
+    for (int i = 1; i <= s.size(); ++i)
+        for (int j = 1; j <= t.size(); ++j)
+            if (s[i - 1] == t[j - 1])
+                dpl[i][j] = 1 + dpl[i - 1][j - 1];
+    for (int i = s.size(); i > 0; --i)
+        for (int j = t.size(); j > 0; --j)
+            if (s[i - 1] == t[j - 1])
+                dpr[i - 1][j - 1] = 1 + dpr[i][j];
+    for (int i = 0; i < s.size(); ++i)
+        for (int j = 0; j < t.size(); ++j)
+            if (s[i] != t[j])
+                res += (dpl[i][j] + 1) * (dpr[i + 1][j + 1] + 1);
+    return res;
+}
 };
