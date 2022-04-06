eda3ae5311dbdf184c44df62f2e193000b00d9e4
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-04-07 02:59:54 +0530

    Time: 503 ms (35.33%), Space: 127 MB (12.02%) - LeetHub

--- 2227-encrypt-and-decrypt-strings/2227-encrypt-and-decrypt-strings.cpp
+++ 2227-encrypt-and-decrypt-strings/2227-encrypt-and-decrypt-strings.cpp
@@ -1,39 +1,32 @@
 class Encrypter {
 public:
-    unordered_map<char,string> m;
-    unordered_map<string,int> dict;
-    // vector k is keys, vector v is values and vector d is dictionary
-    Encrypter(vector<char>& k, vector<string>& v, vector<string>& d) {
-        int n = k.size();
-        // we store the key value pairs in an unordered_map m, so that encryption of a char takes O(1).
-        for(int i=0;i<n;i++){
-            m[k[i]] = v[i];
+    unordered_map<char,string> mp1;\
+    vector<string> dic;
+    Encrypter(vector<char>& keys, vector<string>& values, vector<string>& dictionary) {
+        int n=keys.size();
+        for(int i=0;i<n;i++)
+        {
+            mp1[keys[i]]=values[i];
         }
-        
-        // we encrypt the strings in the vector d and store it's count in an unordered_map dict.
-        // so that we can directly say the count of decryptions for a string in O(1).
-        n = d.size();
-        for(int i=0;i<n;i++){
-            string p = encrypt(d[i]);
-            dict[p]++;
-        }
+        dic=dictionary;
     }
-    
-    string encrypt(string s) {
-        // encrypt the string using key value pairs in unordered_map m
-        int n = s.length();
-        string p = "";
-        for(int i=0;i<n;i++){
-            if(m.find(s[i])==m.end()){
-                return "";
-            }
-            p += m[s[i]];
+    string encrypt(string word1) {
+     string ans="";
+        for(char ch:word1)
+        {
+            if(mp1.find(ch)==mp1.end())
+            return "";
+            ans+=mp1[ch];
         }
-        return p;
+        return ans;
     }
-    
-    int decrypt(string s) {
-        if(dict.find(s)==dict.end()) return 0; // if not present in dict, return 0
-        return dict[s]; // else return count.
+    int decrypt(string word2) {
+        int ans=0;
+        for(auto str:dic)
+        {
+          if(encrypt(str)==word2)
+          ans++;
+        }
+        return ans;
     }
 };
