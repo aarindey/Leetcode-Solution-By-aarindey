ebf74a7e3fea136cfe252d6d9d536f073b571389
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-04-06 06:41:34 +0530

    Time: 1185 ms (23.47%), Space: 10.6 MB (47.62%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,36 +1,14 @@
 class Solution {
 public:
-    long long int mod=1e9+7;
     int threeSumMulti(vector<int>& arr, int target) {
-        unordered_map<int,long> mp;
-        for(int &x:arr)
-        {
-            mp[x]++;
-        }
-        long long int ans=0;
-        for(auto &pr:mp)
-        {
-            int i=pr.first;
-            for(auto &pr2:mp)
-            {
-                int j=pr2.first;
-                int k=target-i-j;
-                if(k<0||k>100||mp.find(k)==mp.end())
-                continue;
-                if(i==j&&j==k)
-                {
-                    ans+=mp[i]*(mp[i]-1)*(mp[i]-2)/6;
-                }
-                else if(i==j&&j!=k)
-                {
-                    ans+=mp[k]*mp[i]*(mp[i]-1)/2;
-                }
-                else if(i<j&&j<k)
-                {
-                    ans+=mp[i]*mp[j]*mp[k];
-                }
+        unordered_map<int, int> m;
+        int res = 0, mod = 1e9 + 7;
+        for (int i = 0; i < arr.size(); i++) {
+            res = (res + m[target - arr[i]]) % mod;
+            for (int j = 0; j < i; j++) {
+                m[arr[i] + arr[j]]++;
             }
         }
-        return (int)(ans%mod);
+        return res;
     }
 };
