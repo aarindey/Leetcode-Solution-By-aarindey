caefeba0705747a9ce4f6d703cc8587b4caeba9d
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-04-06 08:21:08 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (88.92%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int dp[101][101];
-    bool helper(string s1,string s2,string s3,int n1,int n2,int n3,int i,int j,int k)
-    {
-        if(k==n3)
-        return true;
-        if(dp[i][j]!=-1)
-        return dp[i][j];
-        bool case1,case2;
-        case1=case2=false;
-        if(s1[i]==s3[k])
-        case1=helper(s1,s2,s3,n1,n2,n3,i+1,j,k+1);
-        if(s2[j]==s3[k])
-        case2=helper(s1,s2,s3,n1,n2,n3,i,j+1,k+1);
-        return dp[i][j]=case1||case2;
-    }
-    bool isInterleave(string s1, string s2, string s3) {
-        int n1=s1.length(),n2=s2.length(),n3=s3.length();
-        if(n1+n2!=n3)
+     bool isInterleave(string s1, string s2, string s3) {
+    
+    if(s3.length() != s1.length() + s2.length())
         return false;
-        memset(dp,-1,sizeof(dp));
-        return helper(s1,s2,s3,n1,n2,n3,0,0,0);
-    }
+    
+    bool dp[s1.length()+1][s2.length()+1];
+    
+    for(int i=0; i<s1.length()+1; i++)
+        for(int j=0; j< s2.length()+1; j++){
+            if(i==0 && j==0)
+                dp[i][j] = true;
+            else if(i == 0)
+                dp[i][j] = ( dp[i][j-1] && s2[j-1] == s3[i+j-1]);
+            else if(j == 0)
+                dp[i][j] = ( dp[i-1][j] && s1[i-1] == s3[i+j-1]);
+            else
+                dp[i][j] = (dp[i-1][j] && s1[i-1] == s3[i+j-1] ) || (dp[i][j-1] && s2[j-1] == s3[i+j-1] );
+        }
+        
+    return dp[s1.length()][s2.length()];
+}
 };
