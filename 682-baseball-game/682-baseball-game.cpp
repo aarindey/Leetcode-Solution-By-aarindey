cad0d5929a5d0ee03703fce869a47007e69b456f
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-04-10 12:13:07 +0530

    Time: 7 ms (43.28%), Space: 11.2 MB (28.60%) - LeetHub

---
+++ 682-baseball-game/682-baseball-game.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int calPoints(vector<string> &ops)
+    {
+        stack<int> myStack;
+        int score=0;
+        for(int i=0;i<ops.size();i++)
+        {
+            if(ops[i]=="C")
+                myStack.pop();
+            else if(ops[i]=="D")
+                myStack.push(2*myStack.top());
+            else if(ops[i]=="+")
+            {
+                stack<int> temp;
+                temp=myStack;
+                int sum=0;
+                for(int i=1;i<=2;i++)
+                {
+                  sum+=temp.top();
+                    temp.pop();
+                }
+                myStack.push(sum);
+            }
+                             else
+                             {
+                                 int x=stoi(ops[i]);
+                             myStack.push(x);
+           }}
+                             int sum=0;
+                             while(!myStack.empty())
+                             {
+                                 sum+=myStack.top();
+                                 myStack.pop();
+                             }
+                             return sum;
+    }
+};
