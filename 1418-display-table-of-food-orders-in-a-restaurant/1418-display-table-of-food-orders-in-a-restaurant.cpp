905079dc1d0aa40f807e0beb8bb3edc2a4d7c39e
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-04-25 02:53:09 +0530

    Time: 243 ms (55.91%), Space: 43.5 MB (84.09%) - LeetHub

---
+++ 1418-display-table-of-food-orders-in-a-restaurant/1418-display-table-of-food-orders-in-a-restaurant.cpp
@@ -0,0 +1,45 @@
+class Solution 
+{
+public:
+    vector<vector<string>> displayTable(vector<vector<string>>& orders){
+        set<string> s;
+        map<int,vector<string>> mp2;
+        map<string,map<string,int>> mp;
+        int n=orders.size();
+        for(int i=0;i<n;i++)
+        {
+            s.insert(orders[i][2]);
+            mp2[stoi(orders[i][1])].push_back(orders[i][2]);
+            mp[orders[i][1]][orders[i][2]]++;
+        }
+        vector<vector<string>> ans;
+        vector<string> temp;
+        temp.push_back("Table");
+        for(auto it=s.begin();it!=s.end();it++)
+        {
+            temp.push_back(*it);
+        }
+        ans.push_back(temp);
+        
+        for(auto pr:mp2)
+        {
+            temp.clear();
+            temp.push_back(to_string(pr.first));
+            vector<string> v;
+            v=pr.second;
+            for(auto it=s.begin();it!=s.end();it++)
+            {
+                if(find(v.begin(),v.end(),*it)!=v.end())
+                {
+                    temp.push_back(to_string(mp[to_string(pr.first)][*it]));
+                }
+                else
+                {
+                    temp.push_back("0");
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+       }
+};
