1a1906fe940db9dae259261b3fbd31a6d33f4794
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-05-17 14:59:49 +0530

    Added solution - LeetHub

--- Bit Difference - GFG/bit-difference.cpp
+++ Bit Difference - GFG/bit-difference.cpp
@@ -17,13 +17,10 @@ class Solution{
         int ans=0;
         for(int i=0;i<32;i++)
         {
-            if((1<<i&a)!=(1<<i&b))
-            {
-                ans++;
-            }
+            if((a&(1<<i))!=(b&(1<<i)))
+            ans++;
         }
         return ans;
-        
     }
 };
 
