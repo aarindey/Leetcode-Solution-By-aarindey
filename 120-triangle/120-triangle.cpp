2f4a6acb538243ffc2411e2d741b94e21bbf6e72
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-01 17:26:11 +0530

    Time: 17 ms (8.31%), Space: 9 MB (40.49%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
-    int recurse(vector<vector<int>> &triangle,int i,int j,int n,vector<vector<int>> &memo)
-    {
-        if(i==n||j==n)
-        return 0;
-        if(memo[i][j]!=-1)
-        return memo[i][j];
-        int first=0,second=0;
-        if(i+1<n)
-        {
-            first=triangle[i+1][j]+recurse(triangle,i+1,j,n,memo);
-        }
-        if(i+1<n&&j+1<n)
-        {
-            second=triangle[i+1][j+1]+recurse(triangle,i+1,j+1,n,memo);
-        }
-        return memo[i][j]=min(first,second);
-    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>> memo(n,vector<int>(n,-1));
-        int ans=recurse(triangle,0,0,n,memo);
-        
-        return triangle[0][0]+ans;
+        vector<vector<int>> dp(n,vector<int>(n));
+        dp[0][0]=triangle[0][0];
+        int ans=INT_MAX;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                if(i==j)
+                {
+                    dp[i][j]=dp[i-1][j-1]+triangle[i][j];
+                }
+                else if(j==0)
+                {
+                    dp[i][j]=dp[i-1][j]+triangle[i][j];
+                }
+                else 
+                {
+                    dp[i][j]=min(dp[i-1][j-1],dp[i-1][j])+triangle[i][j];
+                }
+                if(i==n-1)
+                {
+                    ans=min(ans,dp[i][j]);
+                }
+            }
+        }
+        if(ans==INT_MAX)
+        return dp[0][0];
+        return ans;
     }
 };
