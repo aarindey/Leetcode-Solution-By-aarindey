cd20c6d48cf7146d9820e6114cac93850a028961
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-01 16:51:06 +0530

    Time: 4 ms (59.80%), Space: 7.8 MB (84.84%) - LeetHub

--- 1024-video-stitching/1024-video-stitching.cpp
+++ 1024-video-stitching/1024-video-stitching.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
-    bool static comp(vector<int> &a,vector<int> &b)
-    {
-        if(a[0]==b[0])
-        {
-            return a[1]<b[1];
-        }
-        return a[0]<b[0];
-    }
     int videoStitching(vector<vector<int>>& clips, int time) {
-        sort(clips.begin(),clips.end(),comp);
+        sort(clips.begin(),clips.end());
         int nextEnd=0;
         int currEnd=0;
         int n=clips.size();
