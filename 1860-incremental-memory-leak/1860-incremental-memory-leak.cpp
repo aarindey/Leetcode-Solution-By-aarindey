9d7a92b6265a750270ae8894e78808fd31d0364f
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-08 05:27:11 +0530

    Time: 4 ms (78.31%), Space: 6.2 MB (11.11%) - LeetHub

---
+++ 1860-incremental-memory-leak/1860-incremental-memory-leak.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> memLeak(int memory1, int memory2) {
+        vector<int> ans;
+        int i=1;
+        int c=1;
+        while(max(memory1,memory2)>=i)
+        {
+            if(memory1>=memory2)
+            {
+                memory1-=i;
+            }
+            else
+            {
+                memory2-=i;
+            }
+            
+            c++;
+            i++;
+        }
+        return {c,memory1,memory2};
+    }
+};
