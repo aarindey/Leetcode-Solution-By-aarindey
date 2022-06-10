57aef425754d3305fdb3ea66139519077f27ab2b
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-10 06:19:36 +0530

    Time: 4 ms (76.02%), Space: 8.8 MB (18.94%) - LeetHub

---
+++ 781-rabbits-in-forest/781-rabbits-in-forest.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        map<int,int> mp;
+        int ans=0;
+        for(auto a:answers)
+        {
+            mp[a]++;
+        }
+        for(auto pr:mp)
+        {
+        
+                int d=pr.second/(pr.first+1);
+                int r=pr.second%(pr.first+1);
+                ans+=d*(pr.first+1);
+                if(r>0)
+                {
+                    ans+=pr.first+1;
+                }   
+            
+        }
+        return ans;
+    }
+};
