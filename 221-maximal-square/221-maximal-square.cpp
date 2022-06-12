cc1cd2ce420e602b0a4d18747a83ff8a6897b641
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-13 00:01:28 +0530

    Time: 152 ms (14.06%), Space: 24.8 MB (5.01%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,27 +1,63 @@
+#define ll long long int
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& matrix) {
-        int n=matrix.size(),m=matrix[0].size();
-        vector<vector<int>> dp(n,vector<int>(m));
-        int maxi=0;
-        for(int i=n-1;i>=0;i--)
+    ll maximumHistogram(vector<ll>& heights)
+      {
+        ll maxi=INT_MIN,n=heights.size();
+        vector<ll> ps(n,-1),ns(n,n);
+        stack<ll> s;
+        for(ll i=0;i<n;i++)
         {
-            for(int j=m-1;j>=0;j--)
-            {
-                if(i==n-1||j==m-1)
-                {
-                    dp[i][j]=matrix[i][j]-'0';
-                }
-                else
-                {
-                if(matrix[i][j]=='0')
-                dp[i][j]=0;
-                else
-                dp[i][j]=1+min(dp[i][j+1],min(dp[i+1][j],dp[i+1][j+1]));
-                }
-                maxi=max(maxi,dp[i][j]);
-            }
+          while(!s.empty()&&heights[s.top()]>=heights[i])
+          s.pop();
+          if(s.empty())
+          ps[i]=-1;
+          else
+          ps[i]=s.top();
+          s.push(i);
         }
-        return maxi*maxi;
+        stack<ll> s2;
+        for(ll i=n-1;i>=0;i--)
+        {
+         while(!s2.empty()&&heights[s2.top()]>=heights[i])
+         s2.pop();
+         if(s2.empty())
+         ns[i]=n;
+         else
+         ns[i]=s2.top();
+         s2.push(i);
+        }
+       for(ll i=0;i<n;i++)
+       {
+           ll z=min(ns[i]-ps[i]-1,heights[i]);
+           maxi=max(maxi,z*z);
+       }
+        return maxi;
+    }
+    int maximalSquare(vector<vector<char>>& matrix) {
+    if(matrix.empty())
+    return 0;
+
+    ll n=matrix.size();
+    ll m=matrix[0].size();
+    vector<ll> currRow(m,0);
+    for(ll i=0;i<m;i++)
+    {
+        currRow[i]+=(matrix[0][i]-'0');
+    }
+    int maxi=maximumHistogram(currRow);
+    for(ll i=1;i<n;i++)
+    {
+        for(ll j=0;j<m;j++)
+        {
+            if(matrix[i][j]=='1')
+            currRow[j]+=1;
+            else
+            currRow[j]=0;
+        }
+        int maxArea=maximumHistogram(currRow);
+        maxi=max(maxi,maxArea);
+    }
+    return maxi;
     }
 };
