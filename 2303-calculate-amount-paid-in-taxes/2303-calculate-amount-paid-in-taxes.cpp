aa77364456768bcf238ec095664241edebba5700
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-12 15:18:24 +0530

    Time: 27 ms (75.00%), Space: 13.2 MB (50.00%) - LeetHub

---
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.cpp
@@ -0,0 +1,37 @@
+#define ll long long int
+class Solution {
+public:
+    double calculateTax(vector<vector<int>>& brackets, int income) {
+        double ans;
+        int n=brackets.size();
+        ll taxable;
+        for(int i=0;i<n;i++){
+            if(i!=0)
+            taxable=brackets[i][0]-brackets[i-1][0];
+            else
+            taxable=brackets[i][0];
+            if(i==0&&income>=taxable)
+            {
+                ll tax=taxable*brackets[i][1];
+                ans+=((double)tax/100.0);
+                income-=brackets[i][0];
+            }
+            else if(i!=0&&income>=taxable)
+            {
+                ll tax=taxable*brackets[i][1];
+                ans+=((double)tax/100.0);
+                income-=taxable;  
+            }
+            else if(income>0)
+            {
+                ans+=((income*brackets[i][1])/100.0);
+                income=0;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return ans;
+    }
+};
