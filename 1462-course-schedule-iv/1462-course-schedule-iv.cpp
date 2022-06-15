dfdcf27811d7f46d9e2dd4f730171325851d2e2e
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-16 03:03:21 +0530

    Time: 338 ms (60.92%), Space: 63.7 MB (63.97%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& p, vector<vector<int>>& q) {
+        vector<vector<bool>> matrix(n,vector<bool>(n,false));
+        if(p.size()==0)
+        {
+            vector<bool> ans(q.size(),false);
+            return ans;
+        }
+        vector<int> indegree(n,0);
+        vector<int> graph[n];
+        for(auto x:p)
+        {
+            graph[x[0]].push_back(x[1]);
+            indegree[x[1]]++;
+        }
+        queue<int> qq;
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+            {
+                qq.push(i);
+            }
+        }
+        vector<int> topo;
+        
+        map<int,int> mp;
+        int i=0;
+        while(!qq.empty())
+        {
+            int ele=qq.front();
+            qq.pop();
+            topo.push_back(ele);
+            mp[ele]=i++;
+            for(auto z:graph[ele])
+            {
+                matrix[ele][z]=true;
+                for(int i=0;i<n;i++) 
+                {
+                   if(matrix[i][ele])
+                   {
+                       matrix[i][z]=true;
+                   }
+                }
+                indegree[z]--;
+                if(indegree[z]==0)
+                {
+                    qq.push(z);
+                }
+            }
+        }
+        vector<bool> ans;
+        for(int i=0;i<q.size();i++)
+        {
+                ans.push_back(matrix[q[i][0]][q[i][1]]);
+        }
+        return ans;
+        
+    }
+};
