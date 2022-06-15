5b5d3f25b882d6da449a7974605f9f4a7d9e0b5e
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-16 03:28:29 +0530

    Time: 234 ms (87.44%), Space: 63.2 MB (67.84%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
-    bool dfs(int source,int destination,vector<int> graph[],vector<bool> &vis)
-    {
-        vis[source]=true;
-        if(source==destination)
-        return true;
-        
-        for(auto x:graph[source])
-        {
-            if(!vis[x]&&dfs(x,destination,graph,vis))
-            return true;
-        }
-        return false;
-    }
     vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& p, vector<vector<int>>& q) {
+        vector<vector<bool>> matrix(n,vector<bool>(n,false));
         if(p.size()==0)
         {
             vector<bool> ans(q.size(),false);
@@ -34,16 +22,21 @@ public:
                 qq.push(i);
             }
         }
-        
-        map<int,int> mp;
         int i=0;
         while(!qq.empty())
         {
             int ele=qq.front();
             qq.pop();
-            mp[ele]=i++;
             for(auto z:graph[ele])
             {
+                matrix[ele][z]=true;
+                for(int i=0;i<n;i++) 
+                {
+                   if(matrix[i][ele])
+                   {
+                       matrix[i][z]=true;
+                   }
+                }
                 indegree[z]--;
                 if(indegree[z]==0)
                 {
@@ -51,26 +44,10 @@ public:
                 }
             }
         }
-       
         vector<bool> ans;
         for(int i=0;i<q.size();i++)
         {
-            int u,v;
-            u=q[i][0];
-            v=q[i][1];
-            vector<bool> vis(n,false);
-            if(!dfs(u,v,graph,vis))
-            {
-                ans.push_back(false);
-            }
-            else if(mp[u]>mp[v])
-            {
-                ans.push_back(false);
-            }
-            else
-            {
-                ans.push_back(true);
-            }
+                ans.push_back(matrix[q[i][0]][q[i][1]]);
         }
         return ans;
         
