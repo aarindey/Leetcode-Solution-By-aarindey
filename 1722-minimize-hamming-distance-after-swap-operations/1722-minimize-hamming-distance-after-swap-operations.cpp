70807568b104d7c09921c3750e2fb72ff506b6b5
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-18 05:06:34 +0530

    Time: 564 ms (59.85%), Space: 125.4 MB (92.02%) - LeetHub

---
+++ 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int findParent(int node,vector<int> &parent)
+    {
+        if(node==parent[node])
+        return node;
+        
+        return parent[node]=findParent(parent[node],parent);
+    }
+    void Union(int x,int y,vector<int> &parent,vector<int> &rank)
+    {
+        x=findParent(x,parent);
+        y=findParent(y,parent);
+        
+        if(x==y)
+        return;
+        
+        if(rank[x]<rank[y])
+        {
+            parent[x]=y;
+        }
+        else if(rank[y]<rank[x])
+        {
+            parent[y]=x;
+        }
+        else
+        {
+            parent[x]=y;
+            rank[y]++;
+        }
+    }
+    int minimumHammingDistance(vector<int>& s, vector<int>& t, vector<vector<int>>& as) {
+        int n=s.size();
+        vector<int> parent(n);
+        vector<int> rank(n,0);
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=i;
+        }
+        for(int i=0;i<as.size();i++)
+        {
+            int x=as[i][0];
+            int y=as[i][1];
+            Union(x,y,parent,rank);
+        }
+        map<int,map<int,int>> store;
+        for(int i=0;i<n;i++)
+        {
+            int x=findParent(i,parent);
+            store[x][s[i]]++;
+        }
+        int hammingDis=0;
+        for(int i=0;i<n;i++)
+        {
+            int x=findParent(i,parent);
+                if(store[x].find(t[i])!=store[x].end()&&store[x][t[i]]>0)
+                {
+                    store[x][t[i]]--;
+                }
+                else
+                {
+                    hammingDis++;
+                }
+            
+        }
+        return hammingDis;
+    }
+};
