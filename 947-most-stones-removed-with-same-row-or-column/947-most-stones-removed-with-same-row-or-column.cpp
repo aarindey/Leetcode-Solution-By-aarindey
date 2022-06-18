f6426e3c59a760795d3e922e11f1b7dd7c01004c
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-18 06:23:56 +0530

    Time: 295 ms (40.54%), Space: 15.4 MB (62.70%) - LeetHub

---
+++ 947-most-stones-removed-with-same-row-or-column/947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int find(int node,vector<int> &parent)
+    {
+        if(node==parent[node])
+        return node;
+        return parent[node]=find(parent[node],parent);
+    }
+    void Union(int x,int y,vector<int> &parent,vector<int> &rank)
+    {
+        x=find(x,parent);
+        y=find(y,parent);
+        if(x==y)
+        return;
+        if(rank[x]<rank[y])
+        {
+            parent[x]=y;
+        }
+        else if(rank[y]<rank[x])
+        {
+            parent[y]=x;
+        }
+        else
+        {
+            parent[y]=x;
+            rank[x]++;
+        }
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        int n=stones.size();
+        vector<int> parent(n),rank(n,0);
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=i;
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(i==j)
+                continue;
+                
+                if(stones[i][0]==stones[j][0]||stones[i][1]==stones[j][1])
+                {
+                    int x=find(i,parent);
+                    int y=find(j,parent);
+                    if(x!=y)
+                    Union(x,y,parent,rank);
+                }
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            cout<<find(i,parent)<<endl;
+        }
+        map<int,int> mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[parent[i]]++;
+        }
+        return n-mp.size();
+    }
+};
