4b262b6e5fc97da11dcd5ea7d29de8a4305c4e35
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-21 23:25:06 +0530

    Time: 941 ms (32.74%), Space: 177.2 MB (6.40%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -27,35 +27,31 @@ public:
     }
     int minCostConnectPoints(vector<vector<int>>& p) {
         // vector<vector<int> > vec;
-        int i=0,n=p.size(),d;
-        vector<int> parent(n),rank(n,0);
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>> > pq;
+      priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+        int ans=0;
+        int n=p.size();
+        int c=0;
+        vector<int> par(n),rank(n,0);
         for(int i=0;i<n;i++)
-        parent[i]=i;
+        {
+            par[i]=i;
+        }
         for(int i=0;i<n;i++)
         {
             for(int j=i+1;j<n;j++)
             {
-                d=(abs(p[i][0]-p[j][0])+abs(p[i][1]-p[j][1]));
+                int d=abs(p[i][0]-p[j][0])+abs(p[i][1]-p[j][1]);
                 pq.push({d,i,j});
             }
         }
-        // sort(vec.begin(),vec.end());
-        int ans=0;
-        int c=0;
         while(!pq.empty())
         {
-            vector<int> v;
-            v=pq.top();
+            vector<int> ele=pq.top();
             pq.pop();
-            if(findParent(v[1],parent)==findParent(v[2],parent))
+            if(findParent(ele[1],par)!=findParent(ele[2],par))
             {
-                continue;
-            }
-            else
-            {
-                join(v[1],v[2],parent,rank);
-                ans+=v[0];
+                join(ele[1],ele[2],par,rank);
+                ans+=ele[0];
                 c++;
             }
             if(c==n-1)
