63e33c0cdd68e5b7c94978c94d9419403d22e9f1
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-24 04:34:38 +0530

    Time: 4 ms (37.23%), Space: 6.1 MB (99.28%) - LeetHub

---
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string reverseParentheses(string s) {
+        stack<int> st;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(')
+            {
+                st.push(i);
+            }
+            else if(s[i]==')')
+            {
+                int low=st.top();
+                st.pop();
+                int high=i;
+                reverse(s.begin()+low+1,s.begin()+high);
+            }
+        }
+        string ans;
+        for(char &c:s)
+        {
+            if(c!='('&&c!=')')
+            {
+                ans.push_back(c);
+            }
+        }
+        return ans;
+    }
+};
