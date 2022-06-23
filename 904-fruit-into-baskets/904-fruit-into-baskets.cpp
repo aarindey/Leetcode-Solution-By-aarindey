f8601b1e55a12cc0f2d4bf6aaa61310ddc395b13
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-24 03:34:07 +0530

    Time: 1754 ms (5.00%), Space: 129.8 MB (5.52%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.cpp
+++ 904-fruit-into-baskets/904-fruit-into-baskets.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
-    int totalFruit(vector<int>& fruits) {
-        int n=fruits.size();
-        int i,j;
-        i=j=0;
-        unordered_map<int,int> mp;
-        int ans=0;
-        while(j<n)
+    int n;
+    bool func(vector<int> &arr,int k)
+    {
+        unordered_map<int,int> mpp;
+        for(int i=0;i<k;i++)
         {
-            mp[fruits[j]]++;
-            while(mp.size()>2)
+            mpp[arr[i]]++;
+        }
+        if(mpp.size()<=2)return 1;
+        for(int i=k;i<n;i++)
+        {
+            mpp[arr[i]]++;
+            mpp[arr[i-k]]--;
+            if(mpp[arr[i-k]]==0)mpp.erase(arr[i-k]);
+            if(mpp.size()<=2)return 1;
+        }
+        return 0;
+    }
+    
+    int totalFruit(vector<int>& arr) 
+    {
+        n=arr.size();
+        int low=0,high=n;
+        int ans=0;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(func(arr,mid))
             {
-                mp[fruits[i]]--;
-                if(mp[fruits[i]]==0)
-                mp.erase(fruits[i]);
-                i++;
+                ans=mid;
+                low=mid+1;
             }
-            ans=max(ans,j-i+1);
-            j++;
+            else
+                high=mid-1;
         }
         return ans;
     }
