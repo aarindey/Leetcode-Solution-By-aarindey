d51cc2081e24e143c583ce0f34bda293ff43c437
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-24 21:13:37 +0530

    Time: 127 ms (52.63%), Space: 18.4 MB (28.49%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,36 +1,23 @@
 #define ll long long int
 class Solution {
 public:
-    int coinChange(vector<int>& v, int sum) {
-        int n=v.size();
-       vector<vector<ll>> dp(n+1,vector<ll>(sum+1,0));
-for(ll i=1;i<=sum;i++)
-{
-    dp[0][i]=INT_MAX-1;
-}
-for(ll i=1;i<=sum;i++)
-{
-    if(i%v[0]==0)
-    {
-        dp[1][i]=i/v[0];
-    }
-    else{
-        dp[1][i]=INT_MAX-1;
-    }
-}
-for(ll i=2;i<=n;i++)
-{
-    for(ll j=1;j<=sum;j++)
-    {
-        if(j<v[i-1])
+    int coinChange(vector<int>& coins, int amount) {
+        vector<ll> dp(amount+1,INT_MAX);
+        dp[0]=0;
+        ll n=coins.size();
+        for(ll i=0;i<n;i++)
         {
-             dp[i][j]=dp[i-1][j];
+            for(ll j=1;j<=amount;j++)
+            {
+                if(j>=coins[i])
+                {
+                    dp[j]=min(1+dp[j-coins[i]],dp[j]);
+                }
+            }
         }
-        else{
-            dp[i][j]=min(dp[i-1][j],(ll)(1+dp[i][j-v[i-1]]));
-        }
-    }
-}
-        return dp[n][sum]==INT_MAX-1?-1:dp[n][sum];
-    }
+        if(dp[amount]==INT_MAX)
+        return -1;
+        
+        return dp[amount];
+    } 
 };
