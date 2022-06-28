89186574616b5e7654c93493366c515d65d4a8d6
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-28 06:44:03 +0530

    Time: 181 ms (56.67%), Space: 54.9 MB (89.91%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
+        // obsp = old bought state profit
+        // ossp = old sold state profit
+        // nbsp = new bought state profit
+        // nssp = new sold state profit
         int obsp=-prices[0],ossp=0;
          int nbsp=0,nssp=0;
         int n=prices.size();
         for(int i=1;i<n;i++)
         {
-            if(ossp-prices[i]>obsp)
-            {
-                nbsp=ossp-prices[i];
-            }
-            else
-            {
-                nbsp=obsp;
-            }
-            
-            if(obsp+prices[i]-fee>ossp)
-            {
-                nssp=obsp+prices[i]-fee;
-            }
-            else
-            {
-                nssp=ossp;
-            }
+            nbsp=max(ossp-prices[i],obsp);
+            nssp=max(obsp+prices[i]-fee,ossp);
             ossp=nssp;
             obsp=nbsp;
         }
