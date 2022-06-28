8e0c0035cd93f66abd63be14f81a333ae1a00cae
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-28 09:58:45 +0530

    Time: 342 ms (19.69%), Space: 62.9 MB (44.10%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices, int fee) {
-        // obsp = old bought state profit
-        // ossp = old sold state profit
-        // nbsp = new bought state profit
-        // nssp = new sold state profit
-        int obsp=-prices[0],ossp=0;
-         int nbsp=0,nssp=0;
-        int n=prices.size();
-        for(int i=1;i<n;i++)
+    int fun(vector<int> &prices,int i,int bought,int &n,int &fee,vector<int> &dp)
+    {
+        if(i>=n)
+        return 0;
+        
+        if(dp[bought]!=-1)
+        return dp[bought];
+        
+        if(prices[i]<=prices[bought])
         {
-            nbsp=max(ossp-prices[i],obsp);
-            nssp=max(obsp+prices[i]-fee,ossp);
-            ossp=nssp;
-            obsp=nbsp;
+            bought=i;
+            return dp[bought]=fun(prices,i+1,bought,n,fee,dp);
         }
-        return nssp;
+        else
+        {
+            int ans=fun(prices,i+1,bought,n,fee,dp);
+            if(prices[i]-prices[bought]>fee)
+            {
+                ans=max(ans,prices[i]-prices[bought]-fee+fun(prices,i+1,i+1,n,fee,dp));
+            }
+            return dp[bought]=ans;
+        }
+    }
+    int maxProfit(vector<int>& prices, int fee) {
+        int n=prices.size();
+        vector<int> dp(n+1,-1);
+        return fun(prices,0,0,n,fee,dp);
     }
 };
