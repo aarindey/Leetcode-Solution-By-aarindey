5fb264da9db75bfa951b7ac71c232d186b120073
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-06-30 07:37:47 +0530

    Time: 382 ms (69.92%), Space: 26.2 MB (69.72%) - LeetHub

--- 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.cpp
+++ 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.cpp
@@ -2,14 +2,14 @@
 class Solution {
 public:
     long long sellingWood(int m, int n, vector<vector<int>>& prices) {
-        vector<vector<ll>> dp(m+1,vector<ll>(n+1));
+        vector<vector<ll>> dp(m+1,vector<ll>(n+1,0));
         for(ll i=0;i<prices.size();i++)
         {
             dp[prices[i][0]][prices[i][1]]=prices[i][2];
         }
-        for(ll i=0;i<=m;i++)
+        for(ll i=1;i<=m;i++)
         {
-            for(ll j=0;j<=n;j++)
+            for(ll j=1;j<=n;j++)
             {
                 ll ans=0;
                 //considering Vertical divisions
