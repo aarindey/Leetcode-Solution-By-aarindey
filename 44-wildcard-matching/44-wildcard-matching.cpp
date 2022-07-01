fc354b9f1af8b0f82145cfec9716a99f41252528
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-07-01 22:12:45 +0530

    Time: 103 ms (41.48%), Space: 6.9 MB (87.92%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,56 +1,10 @@
 class Solution {
 public:
-//     bool fun(string s,string p,int n,int m,vector<vector<int>> &dp)
-//     {
-//         if(n==0&&m==0)
-//         {
-//             return true;
-//         }
-//         if(m==0)
-//         {
-//             return false;
-//         }
-//         if(n==0)
-//         {
-//             bool flag=true;
-//             for(int i=0;i<m;i++)
-//             {
-//                 if(p[i]!='*')
-//                 {
-//                     flag=false;
-//                     break;
-//                 }
-//             }
-//             return flag;
-//         }
-//         if(dp[n][m]!=-1)
-//         {
-//             return dp[n][m];
-//         }
-        
-//         if(s[n-1]==p[m-1])
-//         {
-//             return dp[n][m]=fun(s,p,n-1,m-1,dp);
-//         }
-//         if(p[m-1]=='?')
-//         {
-//             return dp[n][m]=fun(s,p,n-1,m-1,dp);
-//         }
-//         if(p[m-1]=='*')
-//         {
-//             return dp[n][m]=fun(s,p,n,m-1,dp)||fun(s,p,n-1,m,dp);
-//         }
-//         return dp[n][m]=false;
-//     }
     bool isMatch(string s, string p) {
         int n=s.size();
         int m=p.size();
-        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
-        dp[0][0]=true;
-        for(int i=0;i<=n;i++)
-        {
-            dp[i][0]=false;
-        }
+        vector<bool> prev(m+1,false),curr(m+1,false);
+        prev[0]=curr[0]=true;
         bool flag=true;
         for(int i=0;i<=m;i++)
         {
@@ -58,25 +12,27 @@ public:
             {
                 flag=false;
             }
-            dp[0][i]=flag;
+            prev[i]=flag;
         }
         for(int i=1;i<=n;i++)
         {
+            curr[0]=false;
             for(int j=1;j<=m;j++)
             {
 
                     if(s[i-1]==p[j-1]||p[j-1]=='?')
                     {
-                        dp[i][j]=dp[i-1][j-1];
+                        curr[j]=prev[j-1];
                     }
                     else if(p[j-1]=='*')
                     {
-                        dp[i][j]=dp[i][j-1]||dp[i-1][j];
+                        curr[j]=curr[j-1]||prev[j];
                     }
                     else
-                    dp[i][j]=false;
+                    curr[j]=false;
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
