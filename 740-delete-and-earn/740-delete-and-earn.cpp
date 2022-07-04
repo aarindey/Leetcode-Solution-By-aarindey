584df663c2613b8f937e372fd33d19a58c92e310
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-07-04 07:10:50 +0530

    Time: 32 ms (29.76%), Space: 14.9 MB (31.58%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -3,13 +3,13 @@ public:
     int n;
     int deleteAndEarn(vector<int>& nums) {
         n=nums.size();
-        vector<vector<int>> dp(10001,vector<int>(2,0));
+        int maxi=*max_element(nums.begin(),nums.end());
+        vector<vector<int>> dp(maxi+1,vector<int>(2,0));
         map<int,int> mp;
         for(auto &x:nums)
         {
             mp[x]++;
         }
-        int maxi=*max_element(nums.begin(),nums.end());
         for(int i=1;i<=maxi;i++)
         {
             for(int j=0;j<=1;j++)
@@ -20,14 +20,6 @@ public:
                 dp[i][j]=dp[i-1][0]+((mp.find(i)!=mp.end())?(mp[i]*i):0);
             }
         }
-        // for(int i=0;i<=maxi+1;i++)
-        // {
-        //     for(int j=0;j<2;j++)
-        //     {
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
         return max(dp[maxi][0],dp[maxi][1]);
     }
 };
