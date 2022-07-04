3702ca19a259be19485d8dabd21acfde93b4b18c
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-07-04 06:05:41 +0530

    Time: 7 ms (48.74%), Space: 7.3 MB (86.26%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -6,31 +6,23 @@ public:
     {
         if(sum>=total-sum)
         return true;
-        if(i==j)
-        {
-            if(2*(sum+nums[i])>=total)
-            return true;
-            else
-            return false;
-        }
         if(i>j)
         return false;
-        bool ans=false;
         if(!flag)
         {
             if(check(i+1,j,nums,1,sum+nums[i])||check(i,j-1,nums,1,sum+nums[j]))
             {
-                ans|=true;
+                return true;
             }
         }
         else
         {
             if(check(i+1,j,nums,0,sum)&&check(i,j-1,nums,0,sum))
             {
-                ans|=1;
+                return true;
             }
         }
-        return ans;
+        return false;
     }
     bool PredictTheWinner(vector<int>& nums) {
         n=nums.size();
