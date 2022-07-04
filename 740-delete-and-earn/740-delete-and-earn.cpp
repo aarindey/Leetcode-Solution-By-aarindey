c900ed251d197d8f1b08482ca49283a60cf3ad21
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-07-04 07:26:36 +0530

    Time: 7 ms (98.23%), Space: 12.2 MB (79.68%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    int n;
+    int fun(int maxi,vector<int> &mp,vector<int> &dp)
+    {
+        if(maxi<1)
+        {
+            return 0;
+        }
+        if(dp[maxi]!=-1)
+        {
+            return dp[maxi];
+        }
+        int take=0,nontake=0;
+        take+=maxi*mp[maxi];
+        if(maxi>=2)
+        take+=fun(maxi-2,mp,dp);
+        nontake=fun(maxi-1,mp,dp);
+        return dp[maxi]=max(take,nontake);
+    }
     int deleteAndEarn(vector<int>& nums) {
-        n=nums.size();
         int maxi=*max_element(nums.begin(),nums.end());
-        vector<vector<int>> dp(maxi+1,vector<int>(2,0));
         vector<int> mp(maxi+1,0);
+        vector<int> dp(maxi+1,-1);
         for(auto &x:nums)
         {
             mp[x]++;
         }
-        for(int i=1;i<=maxi;i++)
-        {
-            for(int j=0;j<=1;j++)
-            {
-                if(j==0)
-                dp[i][j]=max(dp[i-1][0],dp[i-1][1]);
-                else
-                dp[i][j]=dp[i-1][0]+(mp[i]*i);
-            }
-        }
-        return max(dp[maxi][0],dp[maxi][1]);
+        return fun(maxi,mp,dp);
     }
 };
