b22fdec4a248df8f6dd3000d4e6ee9b3fa7424b2
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-07-07 08:17:13 +0530

    Time: 35 ms (29.81%), Space: 14.7 MB (35.40%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,24 +1,68 @@
 class Solution {
 public:
-     bool isInterleave(string s1, string s2, string s3) {
-    
-    if(s3.length() != s1.length() + s2.length())
-        return false;
-    
-    bool dp[s1.length()+1][s2.length()+1];
-    
-    for(int i=0; i<s1.length()+1; i++)
-        for(int j=0; j< s2.length()+1; j++){
-            if(i==0 && j==0)
-                dp[i][j] = true;
-            else if(i == 0)
-                dp[i][j] = ( dp[i][j-1] && s2[j-1] == s3[i+j-1]);
-            else if(j == 0)
-                dp[i][j] = ( dp[i-1][j] && s1[i-1] == s3[i+j-1]);
-            else
-                dp[i][j] = (dp[i-1][j] && s1[i-1] == s3[i+j-1] ) || (dp[i][j-1] && s2[j-1] == s3[i+j-1] );
+    unordered_map<string,bool> memo;
+    bool fun(string &s1,string &s2,string &s3,int p1,int p2,int p3,int l1,int l2,int l3)
+    {
+        //Base condition
+        if(p3==l3)
+        {
+            if(p1==l1&&p2==l2)
+            {
+                return true;
+            }
+            return false;
         }
+        //Applying Memo using star separated pointers as key
+        string key=to_string(p1)+"*"+to_string(p2)+"*"+to_string(p3);
+        if(memo.find(key)!=memo.end())
+        return memo[key];
         
-    return dp[s1.length()][s2.length()];
-}
+        // When one of the pointers have reached the end
+        if(p1==l1)
+        {
+            if(s2[p2]==s3[p3])
+            {
+                return memo[key]=fun(s1,s2,s3,p1,p2+1,p3+1,l1,l2,l3);
+            }
+            else
+            {
+                return memo[key]=false;
+            }
+        }
+        if(p2==l2)
+        {
+            if(s1[p1]==s3[p3])
+            {
+                return memo[key]=fun(s1,s2,s3,p1+1,p2,p3+1,l1,l2,l3);
+            }
+            else
+            {
+                return memo[key]=false;
+            }
+        }
+
+        // Otherwise
+        bool ans=false;
+        if(s1[p1]==s3[p3])
+        {
+            ans|=fun(s1,s2,s3,p1+1,p2,p3+1,l1,l2,l3);
+        }
+        if(s2[p2]==s3[p3])
+        {
+            ans|=fun(s1,s2,s3,p1,p2+1,p3+1,l1,l2,l3);
+        }
+        return memo[key]=ans;
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        int l1,l2,l3,p1,p2,p3;
+        p1=p2=p3=0;
+        l1=s1.size();
+        l2=s2.size();
+        l3=s3.size();
+        if(l3!=l1+l2)
+        {
+            return false;
+        }
+        return fun(s1,s2,s3,p1,p2,p3,l1,l2,l3);
+    }
 };
