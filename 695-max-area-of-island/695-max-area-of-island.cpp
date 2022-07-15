186cff23eea138a6bbc3cbf56567ddc322307673
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-07-15 08:28:36 +0530

    Time: 35 ms (38.59%), Space: 27.1 MB (11.23%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,30 +1,48 @@
 class Solution {
 public:
-   int dfs(vector<vector<int>> &grid,int i,int j,vector<vector<bool>> &visited)
-    {
-        if(i<0||j<0||i>=grid.size()||j>=grid[0].size())
-        return 0;
-        if(visited[i][j]==true)
-        return 0;
-        if(grid[i][j]==0)
-        return 0;
-        visited[i][j]=true;
-       return (1+dfs(grid,i+1,j,visited)+dfs(grid,i,j+1,visited)+dfs(grid,i-1,j,visited)+dfs(grid,i,j-1,visited));
-    }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int n=grid.size(),m=grid[0].size();
-        vector<vector<bool>> visited(n,vector<bool> (m,false));
-        int maxi=0;
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
+        vector<int> dx={1,-1,0,0};
+        vector<int> dy={0,0,1,-1};
+        int maxArea=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(grid[i][j]==1&&visited[i][j]==false)
-                {    
-                   maxi=max(maxi,dfs(grid,i,j,visited));
-                }   
-            }   
+                if(grid[i][j]&&!vis[i][j])
+                {
+                    queue<pair<int,int>> q;
+                    q.push({i,j});
+                    vis[i][j]=true;
+                    int area=0;
+                    while(!q.empty())
+                    {
+                        pair<int,int> ele=q.front();
+                        q.pop();
+                        // cout<<ele.first<<" "<<ele.second<<endl;
+                        area++;
+                        //vis[ele.first][ele.second]=true;
+                        for(int i=0;i<4;i++)
+                        {
+                            int new_x=ele.first+dx[i];
+                            int new_y=ele.second+dy[i];
+                            if(new_x<0||new_y<0)
+                            continue;
+                            if(new_x>=n||new_y>=m)
+                            continue;
+                            if(!vis[new_x][new_y]&&grid[new_x][new_y])
+                            {
+                                q.push({new_x,new_y});
+                                vis[new_x][new_y]=true;
+                            }
+                        }
+                    }
+                    maxArea=max(maxArea,area);
+                }
+            }
         }
-        return maxi;
+        return maxArea;
     }
 };
