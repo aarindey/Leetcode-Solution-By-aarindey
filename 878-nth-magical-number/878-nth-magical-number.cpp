edeb28267bdddd437ecb1d6d92378a5718da835c
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-07-16 15:27:45 +0530

    Time: 4 ms (25.77%), Space: 5.9 MB (74.57%) - LeetHub

---
+++ 878-nth-magical-number/878-nth-magical-number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int nthMagicalNumber(int n, int a, int b) {
+        long long int l=1,r=2;
+        long long int lcm=(a*b)/__gcd(a,b);
+        long long mod=1e9+7;
+        while(true){
+            long long int n1=(l/a)+(l/b)-(l/lcm);
+            long long int n2=(r/a)+(r/b)-(r/lcm);
+            if(n<=n2){
+                break; 
+            }else{
+                l=r;
+                r=2*l;
+            }
+        }
+        
+        while(l<=r){
+            long long int mid=l+(r-l)/2;
+            long long int num=(mid/a)+(mid/b)-(mid/lcm);
+            if(num==n and ((mid%a==0) or (mid%b==0))){
+                return mid%mod;
+            }   
+            if(num>=n){
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return -1;
+        
+    }
+};
