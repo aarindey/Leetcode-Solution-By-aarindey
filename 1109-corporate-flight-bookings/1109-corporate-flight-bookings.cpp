d8c0059cf951fdab9b73c36d1ac334394a620343
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-07-21 07:31:32 +0530

    Time: 223 ms (96.89%), Space: 67.8 MB (73.19%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -4,19 +4,13 @@ public:
         vector<int> ans(n,0);
         for(int i=0;i<b.size();i++)
         {
-            int first_idx,last_idx,seats;
-            first_idx=b[i][0]-1;
-            last_idx=b[i][1]-1;
-            seats=b[i][2];
-            ans[first_idx]+=seats;
-            if(last_idx+1<n)
-            {
-                ans[last_idx+1]-=seats;
-            }
+            ans[b[i][0]-1]+=b[i][2];
+            if(b[i][1]<n)
+            ans[b[i][1]]-=b[i][2];
         }
         for(int i=1;i<n;i++)
         {
-            ans[i]=ans[i-1]+ans[i];
+            ans[i]+=ans[i-1];
         }
         return ans;
     }
