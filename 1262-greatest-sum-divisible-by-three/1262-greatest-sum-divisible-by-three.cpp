ed051a7f7535cefd39aacb40d1d5e4e58e4580a8
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-07-22 14:02:40 +0530

    Time: 121 ms (32.93%), Space: 38.4 MB (25.22%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
-    int n;
-    int fun(vector<int> &nums,int i,int sum,int rem,vector<vector<int>> &dp)
-    {
-        if(i>=n)
-        {
-            if(sum%3==0)
-            return 0;
-            else
-            return INT_MIN;
-        }
-        
-        if(dp[i][rem]!=-1)
-        return dp[i][rem];
-        
-        int ans=0;
-        ans=nums[i]+fun(nums,i+1,sum+nums[i],(rem+nums[i]%3)%3,dp);
-        ans=max(ans,fun(nums,i+1,sum,rem,dp));
-        
-        return dp[i][rem]=ans;
-    }
     int maxSumDivThree(vector<int>& nums) {
-        n=nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(4,-1));
-        return fun(nums,0,0,0,dp);
+        int n=nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(3,0));
+        for(int i=1;i<=n;i++)
+        {
+            int sum1,sum2,sum3;
+            sum1=nums[i-1]+dp[i-1][0];
+            sum2=nums[i-1]+dp[i-1][1];
+            sum3=nums[i-1]+dp[i-1][2];
+            dp[i]=dp[i-1];
+            dp[i][sum1%3]=max(dp[i][sum1%3],sum1);
+            dp[i][sum2%3]=max(dp[i][sum2%3],sum2);
+            dp[i][sum3%3]=max(dp[i][sum3%3],sum3);
+        }
+        return dp[n][0];
     }
 };
