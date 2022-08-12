f3a520e508bfda9ef9f06da5dba2d574743c84c9
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-08-12 21:11:11 +0530

    Time: 595 ms (16.46%), Space: 84.4 MB (43.01%) - LeetHub

--- 1690-stone-game-vii/1690-stone-game-vii.cpp
+++ 1690-stone-game-vii/1690-stone-game-vii.cpp
@@ -8,13 +8,12 @@ public:
         {
             return dp[i][j];
         }
-
             return dp[i][j]=max(sum-stones[i]-helper(i+1,j,sum-stones[i],stones,dp),sum-stones[j]-helper(i,j-1,sum-stones[j],stones,dp));
         
         return 0;
     }
     int stoneGameVII(vector<int>& stones) {
-       int sum=accumulate(stones.begin(),stones.end(),0);
+        int sum=accumulate(stones.begin(),stones.end(),0);
         int n=stones.size();
         vector<vector<int>> dp(n,vector<int>(n,-1));
         int ans=helper(0,n-1,sum,stones,dp);
