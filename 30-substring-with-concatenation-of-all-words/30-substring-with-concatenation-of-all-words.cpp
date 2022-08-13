685d5f49be2342017caf825421d84e621c387aa5
Arindam Dey <77115128+aarindey@users.noreply.github.com>
2022-08-13 12:06:28 +0530

    Time: 1107 ms (30.77%), Space: 302.5 MB (18.33%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        int n=s.length();
-        int n2=words.size();
-        int len=words[0].size();
-        map<string,int> mp,mp2;
-        for(auto word:words)
-        {
-            mp[word]++;
+        vector<int>ans;
+        int n=words.size();
+        int m=words[0].size();
+        int total=n*m;
+        if(s.size()<total){
+            return ans;
         }
-        vector<int> ans;
-        for(int i=0;i<=n-n2*len;i++)
-        {
-            string str=s.substr(i,n2*len);
-            mp2=mp;
-            for(int j=0;j<n2*len;j+=len)
-            {
-                string str2=str.substr(j,len);
-                //cout<<str2<<endl;
-                mp2[str2]--;
-                if(mp2[str2]==0)
-                {
-                    mp2.erase(str2);
+        for(int i=0;i<=s.size()-total;i++){
+            unordered_map<string,int>mp;
+            
+            for(int j=0;j<words.size();j++){
+                mp[words[j]]++;
+            }
+            int k;
+            for(k=0;k<n;k++){
+                
+                string temp=s.substr(i+k*m,m);
+                // cout<<temp<<" "<<i<<endl;
+                if(mp.count(temp)==0){
+                    break;
+                }
+                else{
+                    if(mp[temp]!=0){
+                        mp[temp]--;
+                    }
+                    else{
+                        break;
+                    }
                 }
             }
-            if(mp2.size()==0)
-            {
+            if(k==n){
                 ans.push_back(i);
             }
+            
         }
-        return ans;
+        return ans ;
     }
 };
